f336808df4aad405949406f7d16aeec0cd025535
Michalis Pagkalos <30598350+mpgl@users.noreply.github.com>
2022-10-13 15:00:07 +0300

    Update __init__.py

--- dendrify/__init__.py
+++ dendrify/__init__.py
@@ -2,3 +2,19 @@ from .equations import library
 from .compartment import Compartment, Soma, Dendrite
 from .neuronmodel import NeuronModel
 from .ephysproperties import EphysProperties
+
+def check_dependencies():
+    """
+    Check if Brian 2 is installed
+    """
+    import sys
+
+    try:
+        import brian2
+    except ImportError as ex:
+        sys.stderr.write(f"Importing brian2 failed: '{ex}'\n")
+        raise ImportError(
+            f"Please install Brian 2 to use dendrify")
+
+
+check_dependencies()
